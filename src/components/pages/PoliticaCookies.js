8abedc5514cc2d8885331bbdbacd77fd636d0d66
Alex Mojica <alexmojicamtz@gmail.com>
2020-07-25 12:15:50 +0200

    Agregado sass, imgs

---
+++ src/components/pages/PoliticaCookies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PoliticaCookies = () => {
+  return (
+    <div>
+      <h1>Esta es la p cookies</h1>
+    </div>
+  );
+};
+
+export default PoliticaCookies;
---
+++ src/components/pages/PoliticaPrivacidad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PoliticaPrivacidad = () => {
+  return (
+    <div>
+      <h1>Esta es la p de privacidad</h1>
+    </div>
+  );
+};
+
+export default PoliticaPrivacidad;
